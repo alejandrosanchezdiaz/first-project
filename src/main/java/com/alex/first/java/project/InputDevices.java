546ed800b884bf95aa6b61b576142446676b8671
Alex <alesandi.uk@gmail.com>
2020-07-03 07:19:18 +0100

    first-class
    Input Devices class created - getter, setter ,toString

---
+++ src/main/java/com/alex/first/java/project/InputDevices.java
@@ -0,0 +1,37 @@
+
+package com.alex.first.java.project;
+
+public class InputDevices {
+    private String typeOfInput;
+    private String brand;
+    
+public InputDevices (String typeOFInput, String brand){
+    this.typeOfInput = typeOfInput;
+    this.brand = brand;
+}
+
+    public String getTypeOfInput() {
+        return typeOfInput;
+    }
+
+    public void setTypeOfInput(String typeOfInput) {
+        this.typeOfInput = typeOfInput;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        return "InputDevices{" + "typeOfInput=" + typeOfInput + ", brand=" + brand + '}';
+    }
+    
+
+
+
+}
--- src/main/java/com/alex/first/java/project/Perifericos.java
+++
@@ -1,7 +0,0 @@
-
-package com.alex.first.java.project;
-
-
-public class Perifericos {
-    
-}
