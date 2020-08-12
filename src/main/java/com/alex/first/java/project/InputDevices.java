74f930ec720b5676c8001626f6cf9bd4b9420770
Alex <alesandi.uk@gmail.com>
2020-08-12 09:58:28 +0200

    last-update
    Some minor changes

--- src/main/java/com/alex/first/java/project/InputDevices.java
+++ src/main/java/com/alex/first/java/project/InputDevices.java
@@ -1,14 +1,14 @@
-
 package com.alex.first.java.project;
 
 public class InputDevices {
+
     private String typeOfInput;
     private String brand;
-    
-public InputDevices (String typeOFInput, String brand){
-    this.typeOfInput = typeOfInput;
-    this.brand = brand;
-}
+
+    public InputDevices(String typeOFInput, String brand) {
+        this.typeOfInput = typeOfInput;
+        this.brand = brand;
+    }
 
     public String getTypeOfInput() {
         return typeOfInput;
@@ -30,8 +30,5 @@ public InputDevices (String typeOFInput, String brand){
     public String toString() {
         return "InputDevices{" + "typeOfInput=" + typeOfInput + ", brand=" + brand + '}';
     }
-    
-
-
 
 }
